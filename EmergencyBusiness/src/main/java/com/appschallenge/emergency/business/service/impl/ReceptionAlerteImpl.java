9db7a9fbc994756f48ade79823b08781eee7a3d8
harabida <harabida@gmail.com>
2015-05-31 22:08:08 +0100

    ajout @Component

--- EmergencyBusiness/src/main/java/com/appschallenge/emergency/business/service/impl/ReceptionAlerteImpl.java
+++ EmergencyBusiness/src/main/java/com/appschallenge/emergency/business/service/impl/ReceptionAlerteImpl.java
@@ -3,6 +3,7 @@ package com.appschallenge.emergency.business.service.impl;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
 import com.appschallenge.emergency.business.dao.IAlerteDao;
 import com.appschallenge.emergency.business.dto.AlerteRecue;
@@ -18,6 +19,7 @@ import com.appschallenge.emergency.business.util.EmergencyException;
  * @author elmehdiharabida
  *
  */
+@Component
 public class ReceptionAlerteImpl implements IReceptionAlerte {
 	@Autowired
 	IAlerteDao alerteDao;
